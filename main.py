c43d616a6a1bb50dae23f205fc7e802fd0624109
Ilya Chekin <98697012+IlyaChekin13@users.noreply.github.com>
2022-11-08 13:15:52 +0300

    Final algotithm 	modified:   HISTORY.txt 	modified:   main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import math
 from signal import Signal
 import numpy as np
 import os
+from datetime import datetime
+
+
+info = "Введите номер команды\n \
+1 - Линейная свертка дискретных сигналов\n \
+2 - Алгоритм быстрого вычисления свертки сигналов на основе ДПФ\n \
+3 - Ввести новые сигналы\n \
+-1 - Выход"
 
 def inverse_discrete_Fourier_transform(signal: list, base: int) -> list:
     "Returns linear convolution of signals! Ура!"
@@ -23,45 +31,69 @@ def mult(dpf_1, dpf_2):
     
     return dpf_3 
 
+def write_to_file(massage):
+    with open("HISTORY.txt", "a") as f:
+        f.write(massage)
+
 def main():
+    now = datetime.now()
+    write_to_file("Сессия от " + now.strftime("%d/%m/%Y %H:%M:%S") + ":\n")
     os.system("clear")
+    cycle_count = 0
+    signal1 = False
+    keep_signals = True
+    usr_inp = 0
     while True:
-        print("Введите номер команды\n \
-1 - Линейная свертка дискретных сигналов\n \
-2 - Алгоритм быстрого вычисления свертки сигналов на основе ДПФ\n \
--1 - Выход")
-        usr_inp = int(input())
+
         if usr_inp == -1:
+            write_to_file("Конец сессии.\n\n")
             break
-        print("Введите первый сигнал\nдля завершения нажмите 'ввод'")
-        signal1 = list()
-        while True:
-            inp = input()
-            if inp == "":
-                break
-            signal1.append(int(inp))
-        signal1 = Signal(signal1)
-        print("Введите второй сигнал\nдля завершения нажмите 'ввод'")
-        signal2 = list()
-        while True:
-            inp = input()
-            if inp == "":
-                break
-            signal2.append(int(inp))
-        signal2 = Signal(signal2)
-        if usr_inp == -1:
-            break
-        elif usr_inp == 2:
-            choice = bool(int(input("Желаете выбрать базу сигнала?\n1 - да\n0 - нет")))
+        if usr_inp == 3:
+            keep_signals = True #int(input("Введите 0 - оставить текущие сигналы, 1 - ввести новые.\n"))
+        else:
+            keep_signals = False
+        if keep_signals:
+            write_to_file("Ввод новых сигналов\n")    
+            print("Введите первый сигнал\nдля завершения нажмите 'ввод'")
+            signal1 = list()
+            while True:
+                inp = input()
+                if inp == "":
+                    break
+                signal1.append(int(inp))
+            signal1 = Signal(signal1)
+            write_to_file("Первый сигнал - " + str(signal1) + "\n")
+            print("Введите второй сигнал\nдля завершения нажмите 'ввод'")
+            signal2 = list()
+            while True:
+                inp = input()
+                if inp == "":
+                    break
+                signal2.append(int(inp))
+            signal2 = Signal(signal2)
+            write_to_file("Второй сигнал - " + str(signal2) + "\n")
+        if usr_inp == 2:
+            write_to_file("Алгоритм быстрого вычисления свертки сигналов на основе ДПФ\n")
+            choice = bool(int(input("Желаете выбрать базу сигнала?\n1 - да\n0 - нет\n")))
+            write_to_file(f"Выбор базы сигнала - {choice} \n")
             old_base = signal1.base
             signal1.set_up_signal(signal2.base, choose=choice)
             signal2.set_up_signal(old_base, choose=choice)
+            write_to_file(f"База сигналов равна {len(signal1)} \n")
             dft_1 = signal1.discrete_Fourier_transform()
             dft_2 = signal2.discrete_Fourier_transform()
             signal3 = inverse_discrete_Fourier_transform(mult(dft_1, dft_2), signal1.base)
             print(signal3)
-        elif usr_inp == 1:
-            print(np.convolve(list(signal1), list(signal2)))
+            write_to_file(f"Результат {signal3} \n")
+        if usr_inp == 1:
+            print("Линейная свертка дискретных сигналов")
+            write_to_file("Линейная свертка дискретных сигналов \n")
+            result = np.convolve(list(signal1), list(signal2))
+            print(result)
+            write_to_file(f"Результат {str(result)} \n")
+        print(info)
+        usr_inp = int(input())
+        cycle_count += 1
 
 
 if __name__ == "__main__":
